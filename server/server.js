5c78a684267e3b321f1e3e67b6b407323e1f505f
ThebaultLouis <louis.thebault.ing@gmail.com>
2019-02-10 18:53:11 +0100

    Adding GET /todos

--- server/server.js
+++ server/server.js
@@ -18,6 +18,14 @@ app.post('/todos', (req, res) => {
     }).catch(e => {
       res.status(400).send(e)
     })
+});
+
+app.get('/todos', (req, res)=>{
+  Todo.find().then(todos => {
+    res.send({todos})
+  }).catch(e => {
+    res.status(400).send(e)
+  })
 })
 
 app.listen(3000, () => {
