40203b6686fdc7f94dcac1d97cfd8e862dcf26a8
ThebaultLouis <louis.thebault.ing@gmail.com>
2019-02-10 19:19:08 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectId} = require('mongodb');
 
 
 var {mongoose} = require('./db/mongoose.js')
@@ -26,6 +27,25 @@ app.get('/todos', (req, res)=>{
   }).catch(e => {
     res.status(400).send(e)
   })
+})
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id
+
+  if(!ObjectId.isValid(id)){
+    res.status(400).send()
+  }
+
+  Todo.findById({
+    _id: id
+  }).then(todo => {
+    if (todo){
+      res.send({todo})
+    }
+    else{
+      res.status(404).send()
+    }
+  }).catch(e => res.status(400).send())
 })
 
 app.listen(3000, () => {
